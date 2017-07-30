d6a4984fe3ef45994ccbac0d2ea66f811fbe7ca1
Stefan Beeman <stefan@eldritchmedia.com>
2017-07-30 08:58:54 -0700

    Dumb version of the click handler.

--- 2.js
+++ 2.js
@@ -1,3 +1,19 @@
 $(document).ready(function () {
-  alert('Hello world!');
+  var current_player = 0;
+
+  $('.cell').bind('click', function() {
+    if ($(this).hasClass('playerx') || $(this).hasClass('playero')) {
+      return;
+    }
+    else if (current_player == 0) {
+      $(this).addClass('playerx');
+      $(this).text('X');
+      current_player = 1;
+    }
+    else {
+      $(this).addClass('playero');
+      $(this).text('O')
+      current_player = 0;
+    }
+  })
 });
